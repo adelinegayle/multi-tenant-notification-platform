278e4cff3441c640f940e495f19b0555f3cf3a0c
Adeline Noman <adeline.noman@heroteams.com.au>
2026-02-08 00:02:29 +1100

    chore: bootstrap Spring Boot project and base structure

---
+++ src/main/java/com/onboarding/notifications/NotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.onboarding.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsApplication.class, args);
+	}
+
+}
